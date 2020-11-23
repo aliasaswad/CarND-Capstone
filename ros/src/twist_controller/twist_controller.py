24003a1008e4b06b340263e44bb450717ee6ff74
Alyas Almaawi <alias_aswad@yahoo.com>
2020-11-23 00:40:14 -0600

    update control fun

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -35,7 +35,36 @@ class Controller(object):
 
         self.last_timestamp = None
 
-    def control(self, *args, **kwargs):
+ 
+    def control(self, target_angular_velocity, target_linear_velocity, current_angular_velocity, current_linear_velocity, dbw_enabled):
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
-        return 1., 0., 0.
+        if not dbw_enabled:
+            self.reset()
+            return 0., 0., 0.
+
+        steer = self.steering_controller.get_steering(target_linear_velocity, target_angular_velocity, current_linear_velocity)
+        throttle = 0.
+        brake = 0.
+
+        current_timestamp = rospy.Time.now()
+        if self.last_timestamp != None:
+            dt =  (current_timestamp - self.last_timestamp).nsecs / 1e9
+            cte = target_linear_velocity - current_linear_velocity
+            acceleration = self.throttle_controller.step(cte, dt)
+
+            filtvalue = self.low_pass_filter.filt(acceleration)
+            if self.low_pass_filter.ready:
+                acceleration = self.low_pass_filter.get()
+
+            if acceleration > 0:
+                throttle = acceleration
+            else:
+                brake = self.vehicle_mass * abs(acceleration) * self.wheel_radius
+
+
+        self.last_timestamp = current_timestamp
+
+        rospy.loginfo('SENDING - [throttle,brake,steer]:[{:.4f},{:.4f},{:.4f}], [cA,cL]:[{:.4f},{:.4f}]m [tA, tL]:[{:.4f},{:.4f}]'.format(throttle, brake, steer,current_angular_velocity, current_linear_velocity,target_angular_velocity, target_linear_velocity))
+
+        return throttle, brake, steer
