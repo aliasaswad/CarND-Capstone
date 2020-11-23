ab1ad790b42f410aa3ae2ec7bccfc04e94a64e8c
Alyas Almaawi <alias_aswad@yahoo.com>
2020-11-23 00:36:06 -0600

    check max value and return it

--- ros/src/twist_controller/pid.py
+++ ros/src/twist_controller/pid.py
@@ -15,13 +15,15 @@ class PID(object):
 
     def reset(self):
         self.int_val = 0.0
+        self.last_int_val = 0.0
 
     def step(self, error, sample_time):
+        self.last_int_val = self.int_val
 
         integral = self.int_val + error * sample_time;
         derivative = (error - self.last_error) / sample_time;
-
-        val = self.kp * error + self.ki * integral + self.kd * derivative;
+        y = self.kp * error + self.ki * self.int_val + self.kd * derivative;
+        val = max(self.min, min(y, self.max))
 
         if val > self.max:
             val = self.max
@@ -30,5 +32,7 @@ class PID(object):
         else:
             self.int_val = integral
         self.last_error = error
+        
+        return val
 
         return val
