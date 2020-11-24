45f45fa1be36b3edf7de64f7599ea450760b5cf1
Alyas Almaawi <alias_aswad@yahoo.com>
2020-11-23 20:02:51 -0600

    load classifier init.

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,8 +1,42 @@
+import rospy
 from styx_msgs.msg import TrafficLight
+import tensorflow as tf
+from PIL import Image
+import cv2
+import numpy as np
+from glob import glob
+import os
+from keras.models import load_model
 
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
+        cwd = os.path.dirname(os.path.realpath(__file__))
+
+        # load keras Lenet style model from file
+        self.class_model = load_model(cwd+'/model.h5')
+        self.class_graph = tf.get_default_graph()
+
+        # detection graph
+        self.dg = tf.Graph()
+        # load 
+        with self.dg.as_default():
+            gdef = tf.GraphDef()
+            with open(cwd+"/models/frozen_inference_graph.pb", 'rb') as f:
+                gdef.ParseFromString( f.read() )
+                tf.import_graph_def( gdef, name="" )
+
+            #get names of nodes. from https://www.activestate.com/blog/2017/08/using-pre-trained-models-tensorflow-go
+            self.session = tf.Session(graph=self.dg )
+            self.image_tensor = self.dg.get_tensor_by_name('image_tensor:0')
+            self.detection_boxes =  self.dg.get_tensor_by_name('detection_boxes:0')
+            self.detection_scores = self.dg.get_tensor_by_name('detection_scores:0')
+            self.detection_classes = self.dg.get_tensor_by_name('detection_classes:0')
+            self.num_detections    = self.dg.get_tensor_by_name('num_detections:0')
+
+        self.tlclasses = [ TrafficLight.RED, TrafficLight.YELLOW, TrafficLight.GREEN ]
+        self.tlclasses_d = { TrafficLight.RED : "RED", TrafficLight.YELLOW:"YELLOW", TrafficLight.GREEN:"GREEN", TrafficLight.UNKNOWN:"UNKNOWN" }
+
         pass
 
     def get_classification(self, image):
